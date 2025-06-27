e577d6afc5052cabd27bc7191ecd3120ebff4e64
zombiee <zombiee.art@gmail.com>
2025-06-27 12:56:20 +0300

    Fix path

---
+++ js/navbox.js
@@ -0,0 +1,14 @@
+(function(){ 
+var nav = '<div id="navlist">\
+  <div class="name"><a href="/pw">Aster\'s PW Tools</a></div>\
+  <div class="links">\
+  <div class="link"><a href="/pw/geniecalculator/">Genie</a></div>\
+  <div class="link"><a href="/pw/refiningsimulator/">Refining</a></div>\
+  <div class="link"><a href="/pw/waravatar/">War Avatar</a></div>\
+  <div class="link"><a href="/pw/starchart/">Star Chart</a></div>\
+  <div class="link"><a href="/pw/homesteadnpcs/">Homestead</a></div>\
+  <div class="link"><a href="/pw/chienkun/">Chienkun</a></div>\
+  <div class="link"><a href="/pw/celestialinfant/">Infant</a></div>\
+  </div></div>';
+document.write(nav);
+})()
